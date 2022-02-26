1113a7d0152e94a17d00fa19c9884479084934ac
Gustavo Luiz <glogustavo4321@gmail.com>
2022-02-26 17:23:29 -0300

    Create Guests.cs
    Criação da classe e de sua variaveis

---
+++ ProjectActvity-CodeFirst/ProjectActvity-CodeFirst/Models/Guests.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace ProjectActvity_CodeFirst.Models
+{
+    public class Guests
+    {
+        [Display(Name = "Guest Code")]
+        public long guestId { get; set; }
+        [Display(Name = "Name of Guest")]
+        public string Name { get; set; }
+        [Display(Name = "e-mail of Guest")]
+        public string EMail { get; set; }
+        [Display(Name = "Guest Phone")]
+        public string Phone { get; set; }
+        [Display(Name = "Will attend")]
+        public bool attendance { get; set; }
+    }
+}
