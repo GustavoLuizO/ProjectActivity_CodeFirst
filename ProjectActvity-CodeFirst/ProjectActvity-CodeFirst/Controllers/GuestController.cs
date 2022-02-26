97f6c7481f27e78f024a10cec2c63c999a10f75c
Gustavo Luiz <glogustavo4321@gmail.com>
2022-02-26 17:26:56 -0300

    Create GuestController.cs
    Create the guestcontroller in the aplication

---
+++ ProjectActvity-CodeFirst/ProjectActvity-CodeFirst/Controllers/GuestController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProjectActvity_CodeFirst.Controllers
+{
+    public class GuestController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
