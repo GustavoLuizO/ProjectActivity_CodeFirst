775ed5cd057186e8619daf3922230153aa932d96
Gustavo Luiz <glogustavo4321@gmail.com>
2022-02-28 11:05:14 -0300

    Criação Da view create

--- ProjectActvity-CodeFirst/ProjectActvity-CodeFirst/Controllers/GuestController.cs
+++ ProjectActvity-CodeFirst/ProjectActvity-CodeFirst/Controllers/GuestController.cs
@@ -1,12 +1,30 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ProjectActvity_CodeFirst.Models;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace ProjectActvity_CodeFirst.Controllers
 {
     public class GuestController : Controller
     {
+        public static IList<Guests> listGuests = new List<Guests>();
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Guests Guest)
+        {
+            listGuests.Add(Guest);
+            Guest.guestId = listGuests.Select(m => m.guestId).Max() + 1;
+            return RedirectToAction("Index");
         }
     }
 }
