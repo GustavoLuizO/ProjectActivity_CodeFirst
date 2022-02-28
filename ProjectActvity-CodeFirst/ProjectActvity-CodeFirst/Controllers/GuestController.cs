dcf05dd2b573e861141f79509e27c1c90e60a3fb
Gustavo Luiz <glogustavo4321@gmail.com>
2022-02-28 11:19:43 -0300

    Update GuestController.cs

--- ProjectActvity-CodeFirst/ProjectActvity-CodeFirst/Controllers/GuestController.cs
+++ ProjectActvity-CodeFirst/ProjectActvity-CodeFirst/Controllers/GuestController.cs
@@ -7,11 +7,21 @@ namespace ProjectActvity_CodeFirst.Controllers
 {
     public class GuestController : Controller
     {
-        public static IList<Guests> listGuests = new List<Guests>();
+        public static IList<Guests> listGuests = new List<Guests>()
+        {
+            new Guests()
+            {
+                guestId = 1,
+                Name = "Exemplo convidado",
+                EMail = "ExemploConvidado@gmail.com",
+                Phone = "3853-1873",
+                attendance = true,
+            }
+        };
 
         public IActionResult Index()
         {
-            return View();
+            return View(listGuests);
         }
 
         public IActionResult Create()
@@ -20,10 +30,10 @@ namespace ProjectActvity_CodeFirst.Controllers
         }
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Create(Guests Guest)
+        public IActionResult Create(Guests guests)
         {
-            listGuests.Add(Guest);
-            Guest.guestId = listGuests.Select(m => m.guestId).Max() + 1;
+            listGuests.Add(guests);
+            guests.guestId = listGuests.Select(m => m.guestId).Max() + 1;
             return RedirectToAction("Index");
         }
     }
